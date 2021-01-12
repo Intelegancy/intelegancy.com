50298c3583a64f04304dc2e17d111c2cdaaf3114
William Daghouz <w.daghouz@gmail.com>
2021-01-12 20:20:31 +0100

    Added dummy content to masthead component.

--- src/components/mastHead.tsx
+++ src/components/mastHead.tsx
@@ -6,6 +6,7 @@ import StartupIllustration from './illustrations/startupIllustration'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
+import Paper from '@material-ui/core/Paper'
 
 
 const useStyles = makeStyles( (theme: Theme) =>
@@ -100,6 +101,50 @@ const Masthead: React.FC = () => {
                     </Button>
 
                 </Grid>
+
+                <Box mt={ 5 } pt={ 5 }>
+
+                    <Grid container spacing={ 5 }>
+
+                        <Grid item xs={ 12 } md={ 4 }>
+
+                            <Typography color='primary' component='h4' gutterBottom variant='h4'> Lorem Ipsum</Typography>
+
+                            <Typography color='textSecondary' component='p' gutterBottom paragraph variant='h5'>
+                                
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam eum voluptate cumque suscipit provident qui repellat maxime vitae consectetur facere!
+                                
+                            </Typography>
+
+                        </Grid>
+
+                        <Grid item xs={ 12 } md={ 4 }>
+
+                            <Typography color='primary' component='h4' gutterBottom variant='h4'> Lorem Ipsum</Typography>
+
+                            <Typography color='textSecondary' component='p' gutterBottom paragraph variant='h5'>
+                                
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam eum voluptate cumque suscipit provident qui repellat maxime vitae consectetur facere!
+                                
+                            </Typography>
+
+                        </Grid>
+
+                        <Grid item xs={ 12 } md={ 4 }>
+
+                            <Typography color='primary' component='h4' gutterBottom variant='h4'> Lorem Ipsum</Typography>
+
+                            <Typography color='textSecondary' component='p' gutterBottom paragraph variant='h5'>
+                                
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam eum voluptate cumque suscipit provident qui repellat maxime vitae consectetur facere!
+                                
+                            </Typography>
+
+                        </Grid>
+
+                    </Grid>
+
+                </Box>
 
             </Box>
 
---
+++ src/components/servicesGrid.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react'
+
+
+const ServicesGrid: FC = () => {
+    
+    return (
+    
+        <div>
+            
+        </div>
+    
+    )
+
+}
+
+
+export default ServicesGrid
